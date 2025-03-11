82aaf877cca01c99b016bd892131aa1cfdfc01db
Mai Walsh <maiwalsh@Mais-MacBook-Air.local>
2025-03-11 09:07:09 -0400

    Added Java Consumer configs.

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,17 @@
 # Use an official OpenJDK runtime as a parent image
-FROM openjdk:25-jdk
+FROM maven:3.9.6-eclipse-temurin-21
 
 # Set the working directory in the container
 WORKDIR /app
 
 # Copy the Java file to the container
-COPY Hello.java .
+# COPY Hello.java .
+
+# Add dependencies for Maven and other tools if required
+COPY ./target/HelloWorldKafka-1.0-SNAPSHOT.jar /app/HelloWorldKafka.jar
 
 # Compile the Java program
-RUN javac Hello.java
+# RUN javac Hello.java
 
-# Set the command to run the compiled Java program
-CMD ["java", "HelloWorld"]
+# Set the entry point to run the Java application
+CMD ["java", "-jar", "/app/HelloWorldKafka.jar"]
---
+++ HelloWorldConsumer.java
@@ -0,0 +1,32 @@
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class HelloWorldConsumer {
+    public static void main(String[] args) {
+        String topic = "test-topic"; // Set your Kafka topic
+
+        // Kafka consumer configuration
+        Properties properties = new Properties();
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "broker:29092"); // Kafka broker address
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "test-group");
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+
+        // Create Kafka consumer instance
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+
+        // Subscribe to the topic
+        consumer.subscribe(Collections.singletonList(topic));
+
+        // Poll and consume messages
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(1000));
+            for (ConsumerRecord<String, String> record : records) {
+                System.out.println("Consumed record with key " + record.key() + " and value " + record.value());
+            }
+        }
+    }
+}
